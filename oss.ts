49e3d8d416700eba61fbfd67906e70d41e6e3e65
Marco Antonio <masjr@amazon.com>
2020-05-19 22:18:40 -0300

    FEA: log yml setup file

--- oss.ts
+++ oss.ts
@@ -1,7 +1,31 @@
 // OS Setup
+import * as yaml from 'https://deno.land/std@0.51.0/encoding/yaml.ts'
+import * as log from "https://deno.land/std@0.51.0/log/mod.ts";
+import { readFileStr } from 'https://deno.land/std@0.51.0/fs/mod.ts';
 
-const main = () => {
-  console.log("Work in progress...")
+import { clone, mkdir, pip } from "./commands.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("INFO"),
+  },
+
+  loggers: {
+    default: {
+      level: "INFO",
+      handlers: ["console"],
+    },
+  },
+})
+
+const logger = log.getLogger()
+
+const main = async () => {
+  // TODO: Create CLI
+  const text = await readFileStr('setup.yml');
+  const setupConfig = await yaml.parse(text)
+
+  logger.info(`yaml setup ${JSON.stringify(setupConfig, null, 2)}`)
 }
 
 
